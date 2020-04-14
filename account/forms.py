2bb20efc590e3bb6991333267851e8be9f93148c
Dovan Sanjaya <dovansanjaya24@gmail.com>
2020-04-14 10:30:27 +0700

    add account, permission, and warehouse crud functionality. differentiate navbar if user is superuser. show username in top right corner box

--- account/forms.py
+++ account/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 from .models import PermissionWarehouse, Warehouse, Contact
 
 class ContactForm(forms.ModelForm):
@@ -6,3 +8,21 @@ class ContactForm(forms.ModelForm):
         model = Contact
         exclude = ['owner']
         
+class WarehouseForm(forms.ModelForm):
+    class Meta:
+        model = Warehouse
+        exclude = ['owner']
+
+class PermissionWarehouseForm(forms.ModelForm):
+    class Meta:
+        model = PermissionWarehouse
+        exclude = []
+
+class AccountForm(UserCreationForm):
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    is_superuser = forms.BooleanField(required=False)
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'is_superuser')
--- account/views.py
+++ account/views.py
@@ -1,9 +1,11 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import PermissionWarehouse, Warehouse, Contact
-from .forms import ContactForm
+from .forms import ContactForm, PermissionWarehouseForm, WarehouseForm, AccountForm
 
 # Create your views here.
 def contacts(request):
@@ -13,6 +15,7 @@ def contacts(request):
     }
     return render(request, 'account/contacts.html', context)
 
+# Contact Controller
 class CreateContact(CreateView):
     model = Contact
     form_class = ContactForm
@@ -24,6 +27,7 @@ class CreateContact(CreateView):
             form.instance.owner = self.request.user
         else:
             permission = get_object_or_404(PermissionWarehouse, user=self.request.user)
+            form.instance.owner = permission.warehouse.owner
         return super(CreateContact, self).form_valid(form)
 
 class UpdateContact(UpdateView):
@@ -37,4 +41,85 @@ class DeleteContact(DeleteView):
     success_url = reverse_lazy('account-contacts')
 
 def manage_accounts(request):
-    return render(request, 'account/accounts.html')
+    warehouses = Warehouse.objects.all()
+    permissions = PermissionWarehouse.objects.all()
+    accounts = User.objects.all()
+    context = {
+        'warehouses': warehouses,
+        'permissions': permissions,
+        'accounts': accounts
+    }
+    return render(request, 'account/accounts.html', context)
+
+# Warehouse Controller
+class CreateWarehouse(UserPassesTestMixin, CreateView):
+    model = Warehouse
+    form_class = WarehouseForm
+    template_name_suffix = '_form'
+    success_url = reverse_lazy('account-manage-accounts')
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+    def form_valid(self, form, *args, **kwargs):
+        form.instance.owner = self.request.user
+        return super(CreateVarianProduct, self).form_valid(form)
+
+class UpdateWarehouse(UserPassesTestMixin, UpdateView):
+    model = Warehouse
+    form_class = WarehouseForm
+    template_name_suffix = '_form'
+    success_url = reverse_lazy('account-manage-accounts')
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+class DeleteWarehouse(UserPassesTestMixin, DeleteView):
+    model = Warehouse
+    success_url = reverse_lazy('account-manage-accounts')
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+# Permission Controller
+class CreatePermissionWarehouse(UserPassesTestMixin, CreateView):
+    model = PermissionWarehouse
+    form_class = PermissionWarehouseForm
+    template_name_suffix = '_form'
+    success_url = reverse_lazy('account-manage-accounts')
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+class DeletePermissionWarehouse(UserPassesTestMixin, DeleteView):
+    model = PermissionWarehouse
+    success_url = reverse_lazy('account-manage-accounts')
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+# Account Controller
+class CreateAccount(UserPassesTestMixin, CreateView):
+    model = User
+    form_class = AccountForm
+    template_name = 'account/account_form.html'
+    success_url = reverse_lazy('account-manage-accounts')
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+class UpdateAccount(UserPassesTestMixin, UpdateView):
+    model = User
+    form_class = AccountForm
+    template_name = 'account/account_form.html'
+    success_url = reverse_lazy('account-manage-accounts')
+
+    def test_func(self):
+        return self.request.user.is_superuser
+
+class DeleteAccount(UserPassesTestMixin, DeleteView):
+    model = User
+    success_url = reverse_lazy('account-manage-accounts')
+
+    def test_func(self):
+        return self.request.user.is_superuser
