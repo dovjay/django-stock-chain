7b92e5deebf1942008283248da126abd6566d515
Dovan Sanjaya <dovansanjaya24@gmail.com>
2020-05-07 12:32:32 +0700

    sku not unique, sku change to code, varianproduct field changes, change to bahasa indonesia, list default size in varianproduct, change invoice item field order

--- inventory/forms.py
+++ inventory/forms.py
@@ -6,6 +6,12 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         exclude = ['warehouse', 'supplier']
+        labels = {
+            'name': 'Nama',
+            'sku': 'Kode',
+            'category': 'Kategori',
+            'image': 'Gambar'
+        }
 
     supplier = forms.ModelChoiceField(queryset=Contact.objects.filter(is_supplier=True), required=False)
 
@@ -13,15 +19,33 @@ class ProductSUForm(forms.ModelForm):
     class Meta:
         model = Product
         exclude = ['supplier']
+        labels = {
+            'name': 'Nama',
+            'sku': 'Kode',
+            'category': 'Kategori',
+            'image': 'Gambar',
+            'warehouse': 'Gudang/Toko'
+        }
 
     supplier = forms.ModelChoiceField(queryset=Contact.objects.filter(is_supplier=True), required=False)
 
 class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
-        exclude = []
+        fields = ['name']
+        labels = {
+            'name': 'Nama'
+        }
 
 class VarianProductForm(forms.ModelForm):
     class Meta:
         model = VarianProduct
         exclude = ['product']
+        labels = {
+            'size': 'Ukuran',
+            'product_type': 'Tipe Produk',
+            'product': 'Produk',
+            'stock': 'Stok',
+            'sell_price': 'Harga Jual',
+            'buy_price': 'Harga Beli'
+        }
--- invoice/forms.py
+++ invoice/forms.py
@@ -6,8 +6,19 @@ class InvoiceForm(forms.ModelForm):
     class Meta:
         model = Invoice
         fields = ['status', 'customer', 'due', 'paid_date', 'discount', 'notes', 'total']
+        labels = {
+            'due': 'Jatuh Tempo',
+            'paid_date': 'Tanggal Bayar',
+            'discount': 'Diskon'
+        }
 
 class InvoiceItemForm(forms.ModelForm):
     class Meta:
         model = InvoiceItem
         exclude = ['invoice']
+        labels = {
+            'varian_product': 'Varian Produk',
+            'name': 'Nama',
+            'sku':  'Kode',
+            'price': 'Harga'
+        } 
